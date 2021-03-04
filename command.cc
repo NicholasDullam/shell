6bad409553d05e55df80f1869c69721ebc8c361e
Nicholas Patrick Dullam <ndullam@data.cs.purdue.edu>
2021-03-04 00:08:21 -0500

    \Total:                   0.0 of 70.0\

--- command.cc
+++ command.cc
@@ -123,7 +123,7 @@ void Command::execute() {
             }
 
             args[_simpleCommands[i]->_arguments.size()] = NULL;
-            if (_outFile != NULL) {
+            if (_outFile) {
                 int fd = open((*_outFile).c_str(), O_WRONLY | O_CREAT, 0666);
                 dup2(fd, 1);
             }
