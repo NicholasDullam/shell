1e4b86c33ed2949b59ad075d4349cfe9aa7f0660
Nicholas Patrick Dullam <ndullam@data.cs.purdue.edu>
2021-03-03 21:58:10 -0500

    \Total:                   0.0 of 70.0\

--- command.cc
+++ command.cc
@@ -110,8 +110,7 @@ void Command::execute() {
     for (int i = 0; i < _simpleCommands.size(); i++) {
         ret = fork();
         if (ret == 0) {
-            char** args;
-            args = (char** const) malloc(_simpleCommands[i]->_arguments.size() * sizeof(char*));
+            char** const args = (char** const) malloc(_simpleCommands[i]->_arguments.size() * sizeof(char*));
             for (int j = 0; j < _simpleCommands[i]->_arguments.size(); j++) {
                 args[j] = (*_simpleCommands[i]->_arguments[j]).c_str();
             }
