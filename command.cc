fa1e490dc729357af254a15af667a1f3432a68a1
Nicholas Patrick Dullam <ndullam@data.cs.purdue.edu>
2021-03-03 23:59:48 -0500

    \Total:                   0.0 of 70.0\

--- command.cc
+++ command.cc
@@ -123,7 +123,7 @@ void Command::execute() {
             }
 
             args[_simpleCommands[i]->_arguments.size()] = NULL;
-            int fd = open((*_outFile).c_str(), O_WRONLY | O_CREAT | O_APPEND, 0600);
+            int fd = open((*_outFile).c_str(), O_WRONLY, 0600);
             //dup2(defaultout, 1);
 
             execvp(args[0], args);
