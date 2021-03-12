407bafd36d890ff4896379e4658b0218781feb4c
Nicholas Patrick Dullam <ndullam@data.cs.purdue.edu>
2021-03-11 23:24:49 -0500

    \Part 2 Total:            7.0 of 22.0\

--- command.cc
+++ command.cc
@@ -158,7 +158,7 @@ void Command::execute() {
 
         // Built-in parent function parsing
         if (!strcmp(args[0], "setenv")) {
-            int env = putenv(args[1], args[2], 1);
+            int env = putenv(args[1], args[2]);
             exit(0);
         } else if (!strcmp(args[0], "unsetenv")) {
             int env = unsetenv(args[1]);
