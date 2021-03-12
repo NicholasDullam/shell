ae664d4fddfdf497df717ce206d6aa29512d2fde
Nicholas Patrick Dullam <ndullam@data.cs.purdue.edu>
2021-03-12 14:51:41 -0500

    \Part 2 Total:            10.0 of 22.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -2015,7 +2015,8 @@ YY_RULE_SETUP
   char* iterator = buffer;
   int n = read(pout[0], iterator++, 1);
   *iterator = '\0';
-  n = read(pout[0], iterator+=20, 20);
+  n = read(pout[0], iterator, 20);
+  iterator+=20;
   *iterator = '\0';
   close(pout[0]);
 
@@ -2031,7 +2032,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 146 "shell.l"
+#line 147 "shell.l"
 { 
   remove_character(yytext, '\\');
   yylval.cpp_string = new std::string(yytext);
@@ -2040,7 +2041,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 152 "shell.l"
+#line 153 "shell.l"
 {
   removeStartAndEnd(yytext);
   yylval.cpp_string = new std::string(yytext);
@@ -2049,7 +2050,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 158 "shell.l"
+#line 159 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   yylval.cpp_string = new std::string(yytext);
@@ -2058,10 +2059,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 163 "shell.l"
+#line 164 "shell.l"
 ECHO;
 	YY_BREAK
-#line 2065 "lex.yy.cc"
+#line 2066 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -3078,4 +3079,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 163 "shell.l"
+#line 164 "shell.l"
