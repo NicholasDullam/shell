a30a667e5fc66a5c38fdbae2fb34b18838f819d4
Nicholas Patrick Dullam <ndullam@data.cs.purdue.edu>
2021-03-12 14:13:42 -0500

    \Part 2 Total:            10.0 of 22.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -1988,9 +1988,7 @@ YY_RULE_SETUP
   if (ret == 0) {
       // Execute shell executable
       dup2(pin[0], 0);
-      close(pin[0]);
       dup2(pout[1], 1);
-      close(pout[1]);
       execvp("/proc/self/exe", NULL);
       perror("Error in Child Process");
       exit(1);
@@ -2017,7 +2015,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 132 "shell.l"
+#line 130 "shell.l"
 { 
   remove_character(yytext, '\\');
   yylval.cpp_string = new std::string(yytext);
@@ -2026,7 +2024,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 138 "shell.l"
+#line 136 "shell.l"
 {
   removeStartAndEnd(yytext);
   yylval.cpp_string = new std::string(yytext);
@@ -2035,7 +2033,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 144 "shell.l"
+#line 142 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   yylval.cpp_string = new std::string(yytext);
@@ -2044,10 +2042,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 149 "shell.l"
+#line 147 "shell.l"
 ECHO;
 	YY_BREAK
-#line 2051 "lex.yy.cc"
+#line 2049 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -3064,4 +3062,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 149 "shell.l"
+#line 147 "shell.l"
