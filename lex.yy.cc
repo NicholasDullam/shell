0ee1031c6d31214bcf573140b31f0027f8baa5e5
Nicholas Patrick Dullam <ndullam@data.cs.purdue.edu>
2021-03-23 13:57:24 -0400

    \Total:                   42.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -3243,7 +3243,6 @@ YY_RULE_SETUP
     buffer = getenv(env);
   }
 
-  int size = 1024;
   char* target = (char*) malloc(sizeof(char) * size);
 
   strcpy(target, yytext, (int) (envStart - yytext))
@@ -3259,7 +3258,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 262 "shell.l"
+#line 261 "shell.l"
 {
   removeStartAndEnd(yytext);
   yylval.cpp_string = new std::string(yytext);
@@ -3268,7 +3267,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 268 "shell.l"
+#line 267 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   yylval.cpp_string = new std::string(yytext);
@@ -3277,10 +3276,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 273 "shell.l"
+#line 272 "shell.l"
 ECHO;
 	YY_BREAK
-#line 3284 "lex.yy.cc"
+#line 3283 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -4297,4 +4296,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 273 "shell.l"
+#line 272 "shell.l"
