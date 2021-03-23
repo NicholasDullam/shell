8e7296ff11e0669b47b032d7b92867363199c012
Nicholas Patrick Dullam <ndullam@data.cs.purdue.edu>
2021-03-23 01:33:55 -0400

    \Total:                   41.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -2801,26 +2801,11 @@ YY_RULE_SETUP
   int size = 1024;
   char* buffer = (char*) malloc(sizeof(char) * size);
 
-  if (!strcmp(yytext, "$")) {
-    // Get PID of Shell
-    sprintf(buffer, "%d", getpid());
-  } else if (!strcmp(yytext, "?")) { 
-    // Get last return code
-    buffer = getenv(yytext);
-  } else if (!strcmp(yytext, "!")) {
-    // Get PID of last process
-    buffer = getenv(yytext);
-  } else if (!strcmp(yytext, "_")) {
-    // Last argument in fully expanded previous command
-    buffer = getenv("LAST_ARG");
-  } else if (!strcmp(yytext, "SHELL")) {
-    // Path of Shell
-    buffer = getenv(yytext);
-  } else {
+
     // Env Variable Expansion
     printf("teting");
     buffer = getenv(yytext);
-  }
+  
 
   if (buffer) {
     for (int i = strlen(buffer) - 1; i >= 0; i--) {
@@ -2838,7 +2823,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 257 "shell.l"
+#line 242 "shell.l"
 {
   removeStartAndEnd(yytext);
   yylval.cpp_string = new std::string(yytext);
@@ -2847,7 +2832,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 263 "shell.l"
+#line 248 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   yylval.cpp_string = new std::string(yytext);
@@ -2856,10 +2841,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 268 "shell.l"
+#line 253 "shell.l"
 ECHO;
 	YY_BREAK
-#line 2863 "lex.yy.cc"
+#line 2848 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -3876,4 +3861,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 268 "shell.l"
+#line 253 "shell.l"
