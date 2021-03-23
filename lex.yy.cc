7c9b6254c00b481a715ee564ccf67b89c4bf240c
Nicholas Patrick Dullam <ndullam@data.cs.purdue.edu>
2021-03-23 14:02:34 -0400

    \Total:                   42.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -3246,7 +3246,12 @@ YY_RULE_SETUP
   char* target = (char*) malloc(sizeof(char) * size);
 
   strncpy(target, yytext, (int) (envStart - yytext) - 2);
-  target[(int) (envStart - yytext)] = '\0';
+  target[(int) (envStart - yytext) - 2] = '\0';
+
+  strncpy(target[strlen(target)], buffer, strlen(buffer));
+  target[strlen(target) + strlen(buffer)] = '\0';
+
+  //strncpy(target, yytext[(int) (envStart - yytext) + length], (int) yytext[strlen(yytext)] - envEnd);
   printf("%s", target);
 
   if(buffer) {
@@ -3258,7 +3263,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 261 "shell.l"
+#line 266 "shell.l"
 {
   removeStartAndEnd(yytext);
   yylval.cpp_string = new std::string(yytext);
@@ -3267,7 +3272,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 267 "shell.l"
+#line 272 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   yylval.cpp_string = new std::string(yytext);
@@ -3276,10 +3281,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 272 "shell.l"
+#line 277 "shell.l"
 ECHO;
 	YY_BREAK
-#line 3283 "lex.yy.cc"
+#line 3288 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -4296,4 +4301,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 272 "shell.l"
+#line 277 "shell.l"
