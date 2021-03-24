43c76e8ac50abab6db25dd1d65c99d8702d509ee
Nicholas Patrick Dullam <ndullam@data.cs.purdue.edu>
2021-03-24 14:57:20 -0400

    \Total:                   47.0 of 70.0\

--- y.tab.cc
+++ y.tab.cc
@@ -210,7 +210,7 @@ void expandWildcardsIfNecessary(char* arg) {
   struct dirent * ent;
   while ( (ent = readdir(dir))!= NULL) {
     // Check if name matches
-    if (regexec(ent->d_name, expbuf ) ==0 ) {
+    if (regexec(ent->d_name, re ) ==0 ) {
       // Add argument 
       Command::_currentSimpleCommand->insertArgument(strdup(ent->d_name)); }
     }
