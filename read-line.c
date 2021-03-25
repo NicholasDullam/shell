bef2e7658a04344d44c4a2672bbbee9acf072d8e
Nicholas Patrick Dullam <ndullam@data.cs.purdue.edu>
2021-03-25 15:05:35 -0400

    \Total:                   53.0 of 70.0\

--- read-line.c
+++ read-line.c
@@ -251,6 +251,9 @@ char * read_line() {
 
           // echo line
           write(1, line_buffer, line_length);
+        } else {
+          ch = 7;
+          write(1,&ch,1);
         }
       } else if (ch1==91 && ch2==68) {
         if (cursor_position > 0) {
