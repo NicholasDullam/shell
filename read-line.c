55506e762e71fabad878388d02e8509d4d80ba2d
Nicholas Patrick Dullam <ndullam@data.cs.purdue.edu>
2021-03-25 13:09:37 -0400

    \Total:                   53.0 of 70.0\

--- read-line.c
+++ read-line.c
@@ -243,7 +243,7 @@ char * read_line() {
         }	
 
         // Copy line from history
-        strncpy(line_buffer, history[history_position], strlen(history_position));
+        strncpy(line_buffer, history[history_position], strlen(history[history_position]));
         line_buffer[strlen(history_position)] = '\0';
         line_length = strlen(line_buffer);
         cursor_position = line_length;
