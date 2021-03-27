24a79ec61bd759a4e76db95075e226cf79435ed7
Nicholas Patrick Dullam <ndullam@data.cs.purdue.edu>
2021-03-26 22:18:45 -0400

    \Total:                   52.0 of 70.0\

--- y.tab.cc
+++ y.tab.cc
@@ -238,7 +238,7 @@ void expandWildcard(char* prefix, char* suffix) {
   if (prefix[0] == 0) {
     sprintf(d, "%s", ".");
   } else {
-    sprintf(d, "./%s", prefix);
+    sprintf(d, "%s", prefix);
   }
 
   DIR * dir = opendir(d);
